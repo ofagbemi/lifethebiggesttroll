6c8ea5ee491825ff1d7accf0988984ca279923fa
Jare Fagbemi <ofagbemi@stanford.edu>
2014-09-07 03:52:33 -0400

    Set serve static assets

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Lifethebiggesttroll
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Serve static files
+    config.serve_static_assets = true
   end
 end
